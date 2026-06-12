9bf5710f917f45caaeac52e8fcfeabe78d486599
Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
2026-06-12 14:03:04 +0100

    Re-export widgets in module (#14)

--- src/alc_aiidalab_widgets/widgets/__init__.py
+++ src/alc_aiidalab_widgets/widgets/__init__.py
@@ -1,1 +1,21 @@
 """Module providing reusable and extensible widget implementations."""
+
+from .code_setup import CodeSetupWidget
+from .database import AiiDADatabaseQueryWidget
+from .download import Download
+from .file_handling import FileUploadWidget
+from .loading import LoadingWidget
+from .status import Status
+from .structure import StructureViewWidget
+from .tables import XYZArrayDataTableWidget
+
+__all__ = [
+    "AiiDADatabaseQueryWidget",
+    "CodeSetupWidget",
+    "Download",
+    "FileUploadWidget",
+    "LoadingWidget",
+    "Status",
+    "StructureViewWidget",
+    "XYZArrayDataTableWidget",
+]
