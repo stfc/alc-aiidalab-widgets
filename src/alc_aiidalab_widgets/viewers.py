d69b2a6f44c148cf472ac1c661d6c4efff7999ab
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-07-06 15:17:34 +0100

    Create a basic array data table viewer for 1 and 3 dimensional arrays stored in an ArrayData node.

--- src/alc_aiidalab_widgets/viewers.py
+++ src/alc_aiidalab_widgets/viewers.py
@@ -3,7 +3,10 @@
 from alc_aiidalab_widgets.widgets.file_viewer import SinglefileDataViewer
 from alc_aiidalab_widgets.widgets.process_node_view import ProcessNodeViewerWidget
 from alc_aiidalab_widgets.widgets.structure import StructureViewWidget
-from alc_aiidalab_widgets.widgets.tables import XYZArrayDataTableWidget
+from alc_aiidalab_widgets.widgets.tables import (
+    GenericArrayDataTableWidget,
+    XYZArrayDataTableWidget,
+)
 
 ALC_AIIDA_VIEWER_MAPPING = {
     # Process node type labels
@@ -15,6 +18,7 @@ ALC_AIIDA_VIEWER_MAPPING = {
     "data.core.structure.StructureData.": StructureViewWidget,
     "data.core.singlefile.SinglefileData.": SinglefileDataViewer,
     "data.core.array.trajectory.TrajectoryData.": StructureViewWidget,
+    "data.core.array.ArrayData.": GenericArrayDataTableWidget,
     # Custom redirections
     "xyz_table": XYZArrayDataTableWidget,
 }
