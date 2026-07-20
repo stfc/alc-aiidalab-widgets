af876362c851e8841ea10732bb509d8b8072a82c
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-07-20 10:56:57 +0100

    Update the database query widget to allow node type refinement in search

--- src/alc_aiidalab_widgets/widgets/database.py
+++ src/alc_aiidalab_widgets/widgets/database.py
@@ -38,29 +38,24 @@ class AiiDADatabaseQueryWidget(ipw.VBox, HasTraits):
         self.title = title
         self.query_type = tuple(query)
 
-        qbuilder = QueryBuilder().append((CalcJobNode, WorkChainNode), project="label")
-
         self.drop_down = ipw.Dropdown(
-            options=sorted({"All"}.union({i[0] for i in qbuilder.iterall() if i[0]})),
-            value="All",
-            description="Process Label",
-            disabled=True,
+            options={
+                "All": self.query_type,
+                **{node.__name__.strip("Node"): node for node in self.query_type},
+            },
+            # value="All",
+            description="Node Type:",
+            disabled=False,
             style={"description_width": "120px"},
             layout={"width": "50%"},
         )
         self.drop_down.observe(self.search, names="value")
-
-        # Disable process labels selection if we are not looking for the calculated
-        # structures.
-        def disable_drop_down(change):
-            self.drop_down.disabled = not change["new"] == "calculated"
 
         # Select structures kind.
         self.mode = ipw.RadioButtons(
             options=["all", "uploaded", "calculated"], layout={"width": "25%"}
         )
         self.mode.observe(self.search, names="value")
-        self.mode.observe(disable_drop_down, names="value")
 
         # Date range.
         # Note: there is Date picker widget, but it currently does not work in Safari:
@@ -92,8 +87,8 @@ class AiiDADatabaseQueryWidget(ipw.VBox, HasTraits):
 
         self.results = ipw.Dropdown(layout={"width": "900px"})
         self.results.observe(self._on_select_node, names="value")
-        self.search()
         super().__init__([box, h_line, self.results])
+        self.search()
 
     def search(self, _=None) -> None:
         """Search structures in the AiiDA database."""
@@ -122,25 +117,18 @@ class AiiDADatabaseQueryWidget(ipw.VBox, HasTraits):
         if self.mode.value == "uploaded":
             qbuild2 = (
                 QueryBuilder()
-                .append(self.query_type, project=["id"], tag="search_results")
+                .append(self.drop_down.value, project=["id"], tag="search_results")
                 .append(Node, with_outgoing="search_results")
             )
             processed_nodes = [n[0] for n in qbuild2.all()]
             if processed_nodes:
                 filters["id"] = {"!in": processed_nodes}
-            qbuild.append(self.query_type, filters=filters, tag="search_results")
+            qbuild.append(self.drop_down.value, filters=filters, tag="search_results")
 
         elif self.mode.value == "calculated":
-            if self.drop_down.value == "All":
-                qbuild.append((CalcJobNode, WorkChainNode), tag="calcjobworkchain")
-            else:
-                qbuild.append(
-                    (CalcJobNode, WorkChainNode),
-                    filters={"label": self.drop_down.value},
-                    tag="calcjobworkchain",
-                )
+            qbuild.append((CalcJobNode, WorkChainNode), tag="calcjobworkchain")
             qbuild.append(
-                self.query_type,
+                self.drop_down.value,
                 with_incoming="calcjobworkchain",
                 filters=filters,
                 tag="search_results",
@@ -149,14 +137,14 @@ class AiiDADatabaseQueryWidget(ipw.VBox, HasTraits):
         elif self.mode.value == "edited":
             qbuild.append(CalcFunctionNode)
             qbuild.append(
-                self.query_type,
+                self.drop_down.value,
                 with_incoming=CalcFunctionNode,
                 filters=filters,
                 tag="search_results",
             )
 
         elif self.mode.value == "all":
-            qbuild.append(self.query_type, filters=filters, tag="search_results")
+            qbuild.append(self.drop_down.value, filters=filters, tag="search_results")
 
         qbuild.order_by({"search_results": {"ctime": "desc"}})
         matches = {n[0] for n in qbuild.iterall()}
